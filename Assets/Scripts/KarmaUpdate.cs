0118e18263445cad7a70672661bec4191da44a5b
jsdhwdmaL <yizhou_li20060430@163.com>
2025-02-23 16:15:36 -0500

    added karma

---
+++ Assets/Scripts/KarmaUpdate.cs
@@ -0,0 +1,30 @@
+using TMPro;
+using UnityEngine;
+
+public class KarmaUpdate : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI karmaText;
+    private int num = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //karmaText = GetComponent<TextMeshPro>();
+        Debug.Log(karmaText.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            num++;
+        }
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            num--;
+        }
+        karmaText.text = "KARMA: " + num.ToString();
+        // Debug.Log("Karma is now " + num);
+    }
+}
