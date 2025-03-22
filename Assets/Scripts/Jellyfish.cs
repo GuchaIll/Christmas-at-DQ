01bbf36e2e994af6669412654ee46800b53d96b6
jsdhwdmaL <yizhou_li20060430@163.com>
2025-03-22 17:15:21 -0400

    added darkview for jellyfish

--- Assets/Scripts/Jellyfish.cs
+++ Assets/Scripts/Jellyfish.cs
@@ -1,15 +1,19 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem.Interactions;
 using System;
+using Microsoft.Unity.VisualStudio.Editor;
 
 public class Jellyfish : MonoBehaviour
 {
     private double distance = 10000;
     private double threshold = 500;
+    public UnityEngine.UI.Image darkView;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        darkView.enabled = false;
         // distance = player - jellyfishModel
     }
 
@@ -20,13 +24,16 @@ public class Jellyfish : MonoBehaviour
         //if (distance < threshold) {
             // tentacles move toward player
             // darkness (but can see the pedestrian?)
+            darkView.enabled = true;
             // scream (need sound asset)
             System.Random rnd = new System.Random();
             double pedChance = rnd.NextDouble();
             if (pedChance < 0.33) {
                 // warped pedestrian standing in the road
             }
-
+        }
+        if (Input.GetKeyDown(KeyCode.N)) {
+            darkView.enabled = false;
         }
     }
 }
